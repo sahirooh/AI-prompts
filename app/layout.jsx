d5d2f203d7f1f29307a9f7999a92165949de6a78
sahirooh <sahirooh@gmail.com>
2023-12-08 20:16:48 +0530

    init layout

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+
+import "./global.css";
+
+export const metadata = {
+  title: "AI Prompt",
+  description: "Discover and share AI generated prompts.",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
