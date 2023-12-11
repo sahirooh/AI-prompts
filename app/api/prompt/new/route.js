30eff3529f333c529e3cdb36b7e99e2b14d5446a
sahirooh <sahirooh@gmail.com>
2023-12-11 19:12:50 +0530

    updated prompt route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,11 +1,23 @@
 import { connectToDB } from "@/utils/database";
+import Prompt from "@/models/prompt";
 
 export const POST = async (req) => {
-    const { userId, prompt, tag } = await req.json();
+  const { userId, prompt, tag } = await req.json();
 
-    try {
-        await connectToDB();
-    } catch (error) {
-        
-    }
-}
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response("An error occurred while creating the prompt.", {
+      status: 500,
+    });
+  }
+};
